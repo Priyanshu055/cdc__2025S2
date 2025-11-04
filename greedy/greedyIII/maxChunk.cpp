a9374531f885747011253fba7d376167293c378d
Priyanshu055 <pm9581543@gmail.com>
2025-11-04 23:04:06 +0530

    maxChunk

---
+++ greedy/greedyIII/maxChunk.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+     int max_so_far=0,cnt=0;
+     for(int i=0;i<arr.size();i++){
+        max_so_far=max(max_so_far,arr[i]);
+        if(max_so_far==i) cnt++;
+     }   
+     return cnt;
+    }
+};
